e707b779395fe7c473a213f13b81729c82f84c57
wangluheng328 <57925878+wangluheng328@users.noreply.github.com>
2022-11-08 11:36:23 -0500

    Proj Done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Die from './Die';
+import RollDice from './RollDice';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Die.css';
+
+class Die extends Component {
+    render() {
+        return (
+            // <i> as in icon
+            <i className={`Die fas fa-dice-${this.props.face} ${this.props.rolling && 'shaking'}`} />
+        )
+    }
+}
+
+export default Die;
